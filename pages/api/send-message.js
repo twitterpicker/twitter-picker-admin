1018bf39d2876fdbabecaf1e16ff8196a1e6a565
greyonion404 <twitterpicker111@gmail.com>
2022-09-17 14:54:54 +0600

    console if auth and input calculation are correct

--- pages/api/send-message.js
+++ pages/api/send-message.js
@@ -158,6 +158,11 @@ const sendMessage = async (recipientID, text) => {
     'Authorization': header,
     'Content-Type': 'application/json'
   };
+  return {
+    send_message_endpoint,
+    header,
+    body,
+  }
   console.log("_____________SEND_MESSAGE_____________");
   let result = await axios.post(send_message_endpoint, body, { headers });
   console.log(result);
